1fcaaa35f64146891f30f97bb071bd6125c64bc1
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-04-23 23:06:48 +0530

    Create  SwapNO.java

---
+++ SwapNO.java
@@ -0,0 +1,31 @@
+class SwapNO {
+    public static void main(String[] args) {
+     
+        int a=10;
+        int b=13;
+        int c;
+        //1)creating temp then swap
+        // c=a;
+        //  a=b;
+        // b=c;
+       //2) using + - not temp var
+        // a=a+b;
+        //  b=a-b;
+        // a=a-b;
+        
+         //3) using */ - not temp var
+        // a=a*b;
+        //  b=a/b;
+        // a=a/b;
+       //4) using bitwise operater 
+    //   a=a^b;
+    //   b=a^b;
+    //   a=a^b;
+    //5)using one statement
+    b=a+b-(a=b);
+       
+       
+         System.out.println("A="+a);
+          System.out.println("B="+b);
+    }
+}
