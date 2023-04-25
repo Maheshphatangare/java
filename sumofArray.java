50ffb4ff2b63e5335e0b7452f1b0267c6ee1968a
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-04-25 23:03:08 +0530

    Create sumofArray.java

---
+++ sumofArray.java
@@ -0,0 +1,11 @@
+class sumofArray {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    int Arr[]={1,2,3,4,5};
+    int sum=0;
+     for(int i=0;i<Arr.length;i++){
+         sum=sum+Arr[i];
+   
+    } System.out.println("Hello, World!"+ sum);  
+}
+}
