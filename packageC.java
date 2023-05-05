85ea44873ac76a602b75b8430cdd9d6a83987232
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-05-05 23:21:13 +0530

    Create  packageC.java

---
+++ packageC.java
@@ -0,0 +1,14 @@
+package pack;  
+public class A{  
+  public void msg(){System.out.println("Hello");}  
+}  
+//save by B.java  
+package mypack;  
+import pack.A;  
+  
+class packageC{  
+  public static void main(String args[]){  
+   A obj = new A();  
+   obj.msg();  
+  }  
+}  
