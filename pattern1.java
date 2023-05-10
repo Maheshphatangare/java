9d6a53f342058d5504dec09faaa945f5c8685033
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-05-10 23:48:04 +0530

    pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class pattern1 {
+	// Function to demonstrate pattern
+
+
+	// Driver Function
+	public static void main(String args[])
+	{
+	for(int i=0;i<=5;i++){
+	    for(int j=0;j<=i;j++){
+	        
+	        System.out.print("*");
+	    }
+	    System.out.println();
+	}
+    }
+}
+// *
+// **
+// ***
+// ****
+// *****
+// ******
