6118af4292d242209a912f33eeabaa53b5e80e90
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-05-25 22:25:01 +0530

    patternRombus.java

---
+++ patternRombus.java
@@ -0,0 +1,31 @@
+import java.util.*;
+  
+public class patternRombus {
+    // Function to demonstrate pattern
+    public static void printPattern(int n)
+    {
+        int i, j;
+        int num = 1;
+        // outer loop to handle number of rows
+        for (i = 1; i <= n; i++) {
+            // inner loop to print spaces
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+  
+            // inner loop to print stars
+            for (j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            // printing new line for each row
+            System.out.println();
+        }
+    }
+  
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
