0b4e7f42503147be386f8f3e44ce5113b31aa893
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-06-02 23:08:49 +0530

    pattern5.java

---
+++ pattern5.java
@@ -0,0 +1,24 @@
+       1
+1
+import java.util.Scanner;
+ 
+public class pattern5
+{            
+    public static void main(String[] args) {
+          
+        int n = 5;
+  
+        for (int i = 0; i < n; i++) {
+            int number = 1;
+            System.out.printf("%" + (n - i) * 2 + "s", "");
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("%4d", number);
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+  
+    }
+  
+}
+ 
