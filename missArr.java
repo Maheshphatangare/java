fbcc38cf32dd48c4a958299217bcf3600c14fe46
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-06-14 23:09:49 +0530

    Create missArr.java

---
+++ missArr.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+ 
+class missArr{
+ 
+    // Function to find the missing number
+    public static void findMissing(int arr[], int N)
+    {
+        int i;
+        int temp[] = new int[N + 1];
+        for (i = 0; i <= N; i++) {
+            temp[i] = 0;
+        }
+ 
+        for (i = 0; i < N; i++) {
+            temp[arr[i] - 1] = 1;
+        }
+ 
+        int ans = 0;
+        for (i = 0; i <= N; i++) {
+            if (temp[i] == 0)
+                ans = i + 1;
+        }
+        System.out.println(ans);
+    }
+    // Driver Code
+    public static void main(String[] args)
+    {
+        int arr[] = { 1, 3, 7, 5, 6, 2 };
+        int n = arr.length;
+ 
+        // Function call
+        findMissing(arr, n);
+    }
+}
