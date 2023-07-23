f83bbff1437b7b419d4590dddea89e37b3d3073f
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-07-23 22:16:44 +0530

    Create  Mathpow.java

---
+++ Mathpow .java
@@ -0,0 +1,18 @@
+class Mathpow {
+   public static int pow(int base,int rise){
+       int b=base;
+       int r=rise;
+       int result=1;
+       for(int i=1;i<=rise;i++){
+          result=result*base;
+       }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a=3;
+        int b=2;
+        pow(a,b);
+        System.out.println("power is:"+ pow(a,b));
+    }
+}
