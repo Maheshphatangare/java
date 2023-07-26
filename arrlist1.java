ba3fe8f51dbddac7b7a571a0d0a24717c98098fa
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-07-26 23:54:51 +0530

    Create arrlist1.java

---
+++ arrlist1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class arrlist1 {
+    public static void main(String[] args) {
+        ArrayList obj=new ArrayList();
+        obj.add("A");
+        obj.add("B");
+        obj.add("C");
+        obj.add(1,"D");
+        System.out.println("Hello, World!"+obj);
+    }
+}
