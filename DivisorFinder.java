4439073248d35f34221e81378acdc0d5320df99d
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-08-09 23:49:13 +0530

    Create  DivisorFinder.java

---
+++ DivisorFinder.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DivisorFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Divisors of " + number + " are:");
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
