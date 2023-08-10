20e3a6429dd669b826afbcdee9e2467435a336a2
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-08-10 23:14:07 +0530

    Create arrsort2.java

---
+++ arrsort2.java
@@ -0,0 +1,21 @@
+// Java program to move all negative numbers to the
+// beginning and all positive numbers to the end with
+// constant extra space
+import java.util.*;
+public class arrsort2 {
+	
+	public static void move(int[] arr)
+	{
+		Arrays.sort(arr);
+	}
+
+	// Driver code
+	public static void main(String[] args)
+	{
+		int[] arr = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+		move(arr);
+		for (int e : arr)
+			System.out.print(e + " ");
+	}
+}
+// This article is contributed by aadityapburujwale
