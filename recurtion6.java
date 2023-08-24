ad9eb33ef0cb350e1a3d049da97dc4e516a832c9
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-08-24 22:57:32 +0530

    Create  recurtion6.java

---
+++ recurtion6.java
@@ -0,0 +1,13 @@
+class recurtion6 {
+    public static void fun(int n){
+        if(n==0)
+        return;
+        fun(n-1);
+        System.out.println(n);
+        fun(n-1);
+    }
+    public static void main(String[] args) {
+    fun(3);
+        System.out.println("Hello, World!");
+    }
+}
