16493550510f4c860cb31ce5ca71d46de663b6a2
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-09-14 22:24:29 +0530

    Update alldiviser.java

--- alldiviser.java
+++ alldiviser.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class alldiviser {
    
-//alldivition
+//alldivition2
    
         public static void main(String[] args) {
             System.out.println("Hello, World!");
