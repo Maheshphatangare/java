7a2a64e1450eeab734c0d3f190bf686b83eec79e
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-09-27 23:26:24 +0530

    Create  Permutations.java
    using recursion

---
+++ Permutations.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Permutations {
+     public static void Per(char[] arr, int fi) {
+        if (fi == arr.length - 1) {
+            System.out.println(Arrays.toString(arr));
+            return;
+        }
+
+        for (int i = fi; i < arr.length; i++) {
+            swap(arr, fi, i);
+            Per(arr, fi + 1);
+            swap(arr, fi, i);
+        }
+    }
+
+    public static void swap(char[] arr, int i, int j) {
+       char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+    public static void main(String[] args) {
+        String arr = "abc"; // Replace with your desired array
+        Per(arr.toCharArray(), 0);
+    }
+}
+   
