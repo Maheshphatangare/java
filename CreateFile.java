b244d3c251ed8af8f6a80ee04a1b812ac368cbff
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-10-05 21:24:39 +0530

    Create  CreateFile.java

---
+++ CreateFile.java
@@ -0,0 +1,28 @@
+mport java.io.File;
+
+public class CreateFile {
+
+	public static void main(String[] args) {
+		try {
+			
+			//create file 
+			
+			File f1=new File("D:\\FileHandling\\Batch1.txt");
+			
+			//createNewFile() -it will create new empty file
+			//getName()-it read file name
+			if(f1.createNewFile()) {
+				System.out.println("File"+f1.getName()+"create successfully");
+			}
+			else
+			{
+				System.out.println("File is already exist");
+			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
