6d44f5b2458ade25c478ac7f86667226a61faea7
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-11-08 22:35:19 +0530

    Update IP_Address .java

--- IP_Address .java
+++ IP_Address .java
@@ -2,7 +2,7 @@ import java.net.InetAddress;
 public class IP_Address 
 {
     public static void main(String args[]) throws Exception
-    {
+    { 
         InetAddress IP = InetAddress.getLocalHost();
         System.out.println("IP of my system is := "+IP.getHostAddress());
     }    
