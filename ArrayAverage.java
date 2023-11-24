83dc5ac7a40d89ddabd0f00136f6dab56b65f62d
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-11-24 23:01:39 +0530

    Update ArrayAverage.java

--- ArrayAverage.java
+++ ArrayAverage.java
@@ -3,7 +3,7 @@ package com.javaprogramto.programs.arrays.average;
 public class ArrayAverage {
  
     public static void main(String[] args) {
- 
+   
         // create an array
         int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
  
