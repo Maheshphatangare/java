dfdf2c0b3d043cfc1658f8375f10162a99e3df29
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2023-12-16 21:38:35 +0530

    Update Frequencyofarray .java

--- Frequencyofarray .java
+++ Frequencyofarray .java
@@ -1,6 +1,6 @@
 public class Frequencyofarray {  
     public static void main(String[] args) {  
-        //Initialize array  
+        //Initialize array   
         int [] arr = new int [] {1, 2, 8, 3, 2, 2, 2, 5, 1};  
         //Array fr will store frequencies of element  
         int [] fr = new int [arr.length];  
